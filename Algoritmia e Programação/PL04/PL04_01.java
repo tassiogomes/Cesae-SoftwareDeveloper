b535f7360bfa50e8b1182c15af073843484ee465
Tassio <tassiocagomes@gmail.com>
2023-03-21 20:57:33 +0000

    PL04_01 complete

--- Algoritmia e Programação/PL04/PL04_01.java
+++ Algoritmia e Programação/PL04/PL04_01.java
@@ -10,44 +10,56 @@ class Main {
     Scanner input = new Scanner(System.in);
 
     // Declarar variáveis
-    int n1, n2, resultado;
-    String operacao;
+    double n1, n2, resultado;
+    String operacao, decisao = "y";
     
     // Entrada de dados
-    System.out.println("Introduza o 1º número: \n");
-    n1 = input.nextInt();
-    System.out.println("Introduza o 2º número: \n");
-    n2 = input.nextInt();   
-    System.out.println("--------------------------------------------------");
+
     
     // Controle
     do{
       do{
-          System.out.println("Introduza uma operação + | - | * | / ");
-          operacao = input.next();      
-          System.out.println("--------------------------------------------------");
-            if(operacao != operacao.equals("+") || operacao != operacao.equals("-") || operacao != operacao.equals("*") || operacao != operacao.equals("/")){
-              System.out.println("Operador inválido, tente novamente!");
-            };        
-      }while(operacao != operacao.equals("+") || operacao != operacao.equals("-") || operacao != operacao.equals("*") || operacao != operacao.equals("/"));
+            System.out.println("Introduza o 1º número: \n");
+            n1 = input.nextDouble();
+            System.out.println("Introduza o 2º número: \n");
+            n2 = input.nextDouble();   
+            System.out.println("--------------------------------------------------");
+            System.out.println("Introduza uma operação | + | - | * | / |");
+            operacao = input.next();      
+            System.out.println("--------------------------------------------------");
+            if(!operacao.equals("+") && !operacao.equals("-") && !operacao.equals("*") && !operacao.equals("/")){
+              System.out.println("Operador inválido, tente novamente um desses | + | - | * | / |\n");
+              System.out.println("Gostaria de tentar de novo? (y/n)\n");
+              decisao = input.next();
+          };         
+      }while(!operacao.equals("+") && !operacao.equals("-") && !operacao.equals("*") && !operacao.equals("/"));
     switch(operacao){
       case "+":
         resultado = (n1+n2);
-        System.out.println("A soma (+) dos dois números é: " + resultado);
+        System.out.println("A soma (+) dos dois números é: " + resultado + "\n");
+        System.out.println("Gostaria de tentar de novo? (y/n)\n");
+        decisao = input.next();
         break;
       case "-":
         resultado = (n1-n2);
-        System.out.println("A subtração (-) dos dois números é: " + resultado);
+        System.out.println("A subtração (-) dos dois números é: " + resultado + "\n");
+        System.out.println("Gostaria de tentar de novo? (y/n)\n");
+        decisao = input.next();
         break;
       case "*":
         resultado = (n1*n2);
-        System.out.println("A multiplicação (*) dos dois números é: " + resultado);
+        System.out.println("A multiplicação (*) dos dois números é: " + resultado + "\n");
+        System.out.println("Gostaria de tentar de novo? (y/n)\n");
+        decisao = input.next();
         break;
       case "/":
         resultado = (n1/n2);
-        System.out.println("A subtração (/) dos dois números é: " + resultado);
+        System.out.println("A subtração (/) dos dois números é: " + resultado + "\n");
+        System.out.println("Gostaria de tentar de novo? (y/n)\n");
+        decisao = input.next();
         break;
     }
-    }while (operacao == operacao.equals("+") || operacao == operacao.equals("-") || operacao == operacao.equals("*") || operacao == operacao.equals("/"));
+    }while (!decisao.equals("n"));
+    System.out.println("Obrigado, até a próxima!");
   }
   }
