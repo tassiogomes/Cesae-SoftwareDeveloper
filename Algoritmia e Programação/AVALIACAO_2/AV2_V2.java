c8e8fc5f90ac6e4f3ff85bad786525b0ed590c0a
Tassio <tassiocagomes@gmail.com>
2023-04-27 22:40:34 +0100

    AV2_V2.java method 6

--- Algoritmia e Programação/AVALIACAO_2/AV2_V2.java
+++ Algoritmia e Programação/AVALIACAO_2/AV2_V2.java
@@ -14,9 +14,15 @@ public class AV2_V2{
         System.out.println(" ");
 
         String[][] data = readData(); // receive the matrix provided from readData method
-        
+        functionMenu(data);
 
 
+    } // chavetas main
+
+
+// ------------------------- Functions/Methods ------------------------------------------------*/
+
+    public static void functionMenu(String[][] data){
         // ****** MENU ******
         Scanner input = new Scanner(System.in); // Initializing the scanner
 
@@ -29,14 +35,14 @@ public class AV2_V2{
             System.out.println("4 - Imprimir as informações de um cliente");
             System.out.println("5 - Imprimir os gêneros e os jogos de uma determinada publisher");
             System.out.println("6 - Imprimir o jogo mais caro e os clientes que o cobmpraram");
-            System.out.println("0 - Sair");
+            System.out.println("7 - Sair");
             System.out.print("Escolha uma das opções acima: ");
 
             // declare variables 
             if (input.hasNextInt()) {                       // integer control
                 menuOption = input.nextInt();
 
-                if (menuOption >= 0 && menuOption <= 6) {   // check if the number is within the desired range
+                if (menuOption >= 1 && menuOption <= 7) {   // check if the number is within the desired range
                     switch(menuOption){
 
                         case 1: {
@@ -71,53 +77,44 @@ public class AV2_V2{
                 input.next(); // Descartar a entrada não válida do usuário
             }
 
+        } while (menuOption != 7);
 
-
-        } while (menuOption != 0);
-
-    } // chavetas main
-
-
-// ------------------------- Functions/Methods ------------------------------------------------*/
-
-
-
-
-
-
-public static String [][] readData(){
-
-    // ***this part counts how many lines the file has***
-    String [][] data = null;            // creating an empty data to receive the data
-    try {
-        Scanner input = new Scanner(new File("GameStart_V2.txt"));
-        int lineNum = 0;
-    input.nextLine();                   // not read the header
-        while (input.hasNextLine()) {
-            input.nextLine();
-            lineNum++;                  // count the number of lines
-        }
-        input.close();
-
-        // this part reads the data and stores it in the data
-        data = new String[lineNum][9];  // size of data with the correct number of lines
-        input = new Scanner(new File("GameStart.csv"));
-        input.nextLine();               // not read the header
-        int row = 0;                    // variable to zero because all the lines will have to be traversed
-        while (input.hasNextLine()) {   // read while exists new lines
-            String dataLine = input.nextLine();              // read the next line from the file and store it in "dataLine"
-            String[] fields = dataLine.split(";");     // split the line into fields based on the ";" character and store in "fields"
-            for (int col = 0; col < fields.length; col++) {  // traverse each field of the line
-                data[row][col] = fields[col];                // store each field in the "data" array in the correct position
-            }
-            row++;
-        }
-        input.close();
-    } catch (IOException exc) {
-        System.out.println("Erro ao ler o arquivo: " + exc.getMessage());
     }
-    return data;
-}
+
+
+    public static String [][] readData(){
+
+        // ***this part counts how many lines the file has***
+        String [][] data = null;            // creating an empty data to receive the data
+        try {
+            Scanner input = new Scanner(new File("GameStart_V2.txt"));
+            int lineNum = 0;
+        input.nextLine();                   // not read the header
+            while (input.hasNextLine()) {
+                input.nextLine();
+                lineNum++;                  // count the number of lines
+            }
+            input.close();
+
+            // this part reads the data and stores it in the data
+            data = new String[lineNum][9];  // size of data with the correct number of lines
+            input = new Scanner(new File("GameStart.csv"));
+            input.nextLine();               // not read the header
+            int row = 0;                    // variable to zero because all the lines will have to be traversed
+            while (input.hasNextLine()) {   // read while exists new lines
+                String dataLine = input.nextLine();              // read the next line from the file and store it in "dataLine"
+                String[] fields = dataLine.split(";");     // split the line into fields based on the ";" character and store in "fields"
+                for (int col = 0; col < fields.length; col++) {  // traverse each field of the line
+                    data[row][col] = fields[col];                // store each field in the "data" array in the correct position
+                }
+                row++;
+            }
+            input.close();
+        } catch (IOException exc) {
+            System.out.println("Erro ao ler o arquivo: " + exc.getMessage());
+        }
+        return data;
+    }
 
 // case 1
 // This method takes a two-dimensional array of Strings as input and prints its contents to the console.
