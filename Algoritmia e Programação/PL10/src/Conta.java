0494daa68ed1e9203a2ef02e55517aca1012985e
Tassio <ds.tassio@gmail.com>
2023-05-08 11:40:05 +0100

    PL10_02

--- Algoritmia e Programação/PL10/src/Conta.java
+++ Algoritmia e Programação/PL10/src/Conta.java
@@ -1,18 +1,22 @@
 public class Conta {
 
+
+    /**************** ATRIBUTTES *********************/
     private String numeroConta, titular;
     private double saldo, divida, margemEmprestimo;
     private int anoAbertura;
 
-    public Conta(String numeroConta, String titular, int anoAbertura) {
+    /**************** CONSTRUCTOR *********************/
+    public Conta(String numeroConta, String titular, double saldo) {
         this.numeroConta = numeroConta;
         this.titular = titular;
+        this.saldo = saldo;
         this.anoAbertura = anoAbertura;
-        this.saldo = 0;
         this.divida = 0;
         this.margemEmprestimo = 0;
     }
 
+    /**************** GETTERS AND SETTERS *********************/
     public String getNumeroConta() {
         return numeroConta;
     }
@@ -50,20 +54,23 @@ public class Conta {
         return margemEmprestimo;
     }
 
-    private void atualizarMargemEmprestimo() {
-        this.margemEmprestimo = this.saldo * 0.9;
-    }
 
+    /**************** METHODS *********************/
     public boolean transferencia(double valor, Conta contaDestino) {
         if (this.saldo >= valor) {
             this.saldo -= valor;
             contaDestino.saldo += valor;
-            System.out.println("Transferência concluída.");
+            atualizarMargemEmprestimo();
+            System.out.printf("Transferência concluída no valor de %.2f.\n", valor);
             return true;
         } else {
             System.out.println("Erro ao transferir.");
             return false;
         }
+    }
+
+    public void atualizarMargemEmprestimo() {
+        this.margemEmprestimo = this.saldo * 0.9;
     }
 
     public void depositar(double valor) {
@@ -73,12 +80,25 @@ public class Conta {
     }
 
     public boolean levantar(double valor) {
-        if (this.saldo - valor >= -this.margemEmprestimo) {
+        if (this.saldo - valor >= this.margemEmprestimo) {
             this.saldo -= valor;
+            atualizarMargemEmprestimo();
             System.out.printf("Levantamento de %.2f realizado com sucesso. Saldo atual: %.2f\n", valor, this.saldo);
             return true;
         } else {
             System.out.println("Não há saldo suficiente para realizar o levantamento.");
+            return false;
+        }
+    }
+
+    public boolean pedirEmprestimo(double valor){
+        if(this.margemEmprestimo >= valor && this.divida ==0 ) {
+            this.saldo -= valor;
+            atualizarMargemEmprestimo();
+            System.out.printf("O empréstimo de %.2f realizado com sucesso. Saldo atual: %.2f\n", valor, this.saldo);
+            return true;
+        }else{
+            System.out.println("Não há margem de empréstimo suficiente para realizar o levantamento.");
             return false;
         }
     }
--- Algoritmia e Programação/PL10/src/PL10_02.java
+++ Algoritmia e Programação/PL10/src/PL10_02.java
@@ -2,6 +2,59 @@ public class PL10_02 {
 
     public static void main(String[] args) {
 
+        // Instanciar Contas
+        Conta conta1 = new Conta("111222", "Cristiano Ronaldo", 2022);
+        System.out.println("Margem de Empréstimo C1: "+conta1.getMargemEmprestimo());
+        Conta conta2 = new Conta("222333", "Conor Mcgregor", 2023);
+        System.out.println("Margem de Empréstimo C2: "+conta2.getMargemEmprestimo());
+
+        /******* TRANSFERÊNCIA *************/
+        System.out.println("Teste método transferência");
+        conta1.transferencia(130,conta2);
+        System.out.println("Margem de Empréstimo C1: "+conta1.getMargemEmprestimo());
+        System.out.println("Margem de Empréstimo C2: "+conta2.getMargemEmprestimo());
+        conta2.transferencia(200, conta1);
+        System.out.println("Margem de Empréstimo C1: "+conta1.getMargemEmprestimo());
+        System.out.println("Margem de Empréstimo C2: "+conta2.getMargemEmprestimo());
+        System.out.println("O saldo da conta 1 é: " + conta1.getSaldo() + " e o saldo da conta 2 é : "+ conta2.getSaldo());
+        System.out.println("");
+        System.out.println("-----------------------------------------------------------");
+        System.out.println();
+
+
+        /********* DEPOSITAR **********/
+
+        conta1.depositar(1000);
+        conta2.depositar(2000);
+        System.out.println("Saldo da Conta "+conta1.getNumeroConta()+": "+conta1.getSaldo());
+        System.out.println("Margem de Empréstimo C1: "+conta1.getMargemEmprestimo());
+        System.out.println("Saldo da Conta "+conta2.getNumeroConta()+": "+conta2.getSaldo());
+        System.out.println("Margem de Empréstimo C2: "+conta2.getMargemEmprestimo());
+        System.out.println("");
+        System.out.println("-----------------------------------------------------------");
+        System.out.println();
+
+
+        /*********** LEVANTAR *****************/
+        conta1.levantar(200);
+        conta1.mostrarSaldo();
+        System.out.println("Margem de Empréstimo C1: "+conta1.getMargemEmprestimo());
+        conta2.levantar(700);
+        conta2.mostrarSaldo();
+        System.out.println("Margem de Empréstimo C2: "+conta2.getMargemEmprestimo());
+        System.out.println("");
+        System.out.println("-----------------------------------------------------------");
+        System.out.println();
+
+        /*************** PEDIR EMPRÉSTIMO ****************/
+        conta1.mostrarMargemEmprestimo();
+        conta1.pedirEmprestimo(100);
+        conta1.mostrarSaldo();
+        conta2.mostrarMargemEmprestimo();
+        conta2.pedirEmprestimo(2926);
+        conta2.mostrarSaldo();
+
+
 
 
 
