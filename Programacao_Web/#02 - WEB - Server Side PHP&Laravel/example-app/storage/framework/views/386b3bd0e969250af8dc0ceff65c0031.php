990925e11499b793b3e1c88b6a938cf39300515a
Tassio Gomes <ds.tassio@gmail.com>
2023-05-11 10:03:06 +0100

    laravel

---
+++ Programacao_Web/#02 - WEB - Server Side PHP&Laravel/example-app/storage/framework/views/386b3bd0e969250af8dc0ceff65c0031.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('title', __('Server Error')); ?>
+<?php $__env->startSection('code', '500'); ?>
+<?php $__env->startSection('message', __('Server Error')); ?>
+
+<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\CESAE\Cesae-SoftwareDeveloper\Programacao_Web\#02 - WEB - Server Side PHP&Laravel\example-app\vendor\laravel\framework\src\Illuminate\Foundation\Exceptions/views/500.blade.php ENDPATH**/ ?>
