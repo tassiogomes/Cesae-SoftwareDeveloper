36f739bb69721db10e350f26013cce5ce10204ac
Tassio <ds.tassio@gmail.com>
2023-05-15 12:25:52 +0100

    PL11_01 e PL11_02

--- Algoritmia e Programação/poo_fp11_02/src/poo_fp11_02.java
+++ Algoritmia e Programação/poo_fp11_02/src/poo_fp11_02.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
+
 public class poo_fp11_02 {
     public static void main(String[] args) {
 
+        /** Construtor 1 que instancia uma lista dentro do próprio construtor e recebe como parâmetro uma instância **/
         Farm fazenda = new Farm();
         fazenda.adicionarAnimal(new Cao());
         fazenda.adicionarAnimal(new Gato());
@@ -8,8 +11,18 @@ public class poo_fp11_02 {
         fazenda.adicionarAnimal(new Porco());
         fazenda.adicionarAnimal(new Galinha());
         fazenda.adicionarAnimal(new Pato());
-
+        System.out.println("makeNoise adicionando individualmente");
         fazenda.makeNoise();
+        System.out.println("\n");
+
+        /** Construtor 2 que PRECISA de uma list como parâmetro e essa lista é instanciada aqui na Main **/
+        ArrayList<Animal> listaAnimais = new ArrayList<>();
+        listaAnimais.add(new Cao());
+        listaAnimais.add(new Porco());
+
+        System.out.println("makeNoise adicionando lista");
+        Farm fazenda2 = new Farm(listaAnimais);
+        fazenda2.makeNoise();
 
     }
 }
