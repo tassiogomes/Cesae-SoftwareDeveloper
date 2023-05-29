c0bc997504c6be591f7e6959e36cebb9859f2a47
Tassio <ds.tassio@gmail.com>
2023-05-29 20:28:34 +0100

    verificacao ricardo

--- Algoritmia e Programação/AVALIACAO_3_POO/src/Entidades/Cavaleiro.java
+++ Algoritmia e Programação/AVALIACAO_3_POO/src/Entidades/Cavaleiro.java
@@ -16,7 +16,46 @@ public class Cavaleiro extends Heroi {
      *
      * @param npc
      */
+    @Override
+    public Entidade atacar(NPC npc) {
 
+
+        while (this.getVida() >= 0 && npc.getVida() >= 0) {
+            // O inimigo ataca primeiro (80% da força original)
+            int danoInimigo = (int) (npc.getForca() * 0.8); // sofre apenas 80% do dano porque ele tem armardura
+            this.subtrairVida(danoInimigo); // que é a quantidade passada no parametro da funcao na classe Heroi
+            System.out.println(this.getVida());
+
+            // Verifica se o herói ainda está vivo
+            if (this.getVida() <= 0) {
+                System.out.println("O herói foi derrotado.");
+
+            } else {
+                // O herói ataca
+                int danoHeroi = this.getForca() + this.getArma().getAtaque();
+                npc.setVida(npc.getVida() - danoHeroi);
+                System.out.println(npc.getVida());
+                //npc.subtrairVida(danoHeroi);
+
+                // Verifica se o inimigo ainda está vivo
+                if (npc.getVida() <= 0) {
+                    System.out.println("O herói venceu!");
+                    this.subirNivel();
+                    this.incrementarVida(10);
+                    this.incrementarForca(1);
+                    this.incrementarOuro(10);
+
+                }
+            }
+        }
+        if (npc.getVida() <= 0) {
+            System.out.println("O herói venceu!");
+            return this;
+        } else {
+            return npc;
+        }
+
+    }
 
 
     /**
